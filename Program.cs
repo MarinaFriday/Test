3aa9d80ac9828edfef5168a433776becb35ba920
MarinaFriday <kokorina1995@list.ru>
2022-04-14 20:33:23 +0400

    Change

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");            
+            Console.WriteLine("Изменение");
         }
     }
 }
